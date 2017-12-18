647874a0c0e6899c936421b40df57734f5fcf146
Killerpop <34385441+Killerpop@users.noreply.github.com>
2017-12-18 20:13:56 -0300

    Update evil-deb.py

--- evil-deb.py
+++ evil-deb.py
@@ -60,7 +60,7 @@ def printer():
 (_______/   \_/   \_______/(_______/ (______/ (_______/|/ \___/ 0.6v.
                                                                
                          Malicious Debian Package Creator
-                         WebSite :: http://haxkur1.mozello.com/
+                         WebSite :: http://www.haxkur.tk/
 			     By: @Killerpop
 
 
